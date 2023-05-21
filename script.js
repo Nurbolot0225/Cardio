6f534fa8dd75d5407e277023432317cb05943521
Nurbolot <nurchik0225@gmail.com>
2023-05-22 01:22:30 +0600

    Размещение Формы Ввода Тренировки

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--temp');
-const inputElevation = document.querySelector('.form__input--climb');
+const inputTemp = document.querySelector('.form__input--temp');
+const inputClimb = document.querySelector('.form__input--climb');
+
+let map, mapEvent;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -18,29 +20,17 @@ if (navigator.geolocation) {
 
       console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      map.on('click', function(mapEvent) {
-        console.log(mapEvent);
-
-        const {lat, lng} = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 200,
-              minHeight: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-          }))
-          .setPopupContent('Тренеровка')
-          .openPopup();
+      // Обработка клика на карте
+      map.on('click', function(event) {
+        mapEvent = event;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       })
     },
     function() {
@@ -48,3 +38,32 @@ if (navigator.geolocation) {
     }
   )
 }
+
+form.addEventListener('submit', function(event)  {
+  event.preventDefault();
+
+  // Очистка полей ввода данных
+  inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
+
+  // Отображение маркера
+  console.log(mapEvent);
+  const {lat, lng} = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 200,
+        minHeight: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+    }))
+    .setPopupContent('Тренеровка')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function() {
+  inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+  inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+})
+
