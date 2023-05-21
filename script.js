1532a55c4b2f429473dffa1c038069f24382e3c0
Nurbolot <nurchik0225@gmail.com>
2023-05-21 23:52:36 +0600

    Добавлено карта / библиотека leaflet

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      const {latitude} = position.coords;
+      const {longitude} = position.coords;
+
+      const coords = [latitude, longitude];
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+      L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function() {
+      alert('Что то пошло не так!')
+    }
+  )
+}
