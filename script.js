8ded18c5e7e229213aa44a0cc75746435aef22de
Nurbolot <nurchik0225@gmail.com>
2023-06-01 17:12:39 +0600

    РефакториÐнг кода под АргÐхитектуру Проекта

--- script.js
+++ script.js
@@ -8,62 +8,86 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputTemp = document.querySelector('.form__input--temp');
 const inputClimb = document.querySelector('.form__input--climb');
 
-let map, mapEvent;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function(position) {
+class App {
+
+  #map
+  #mapEvent
+
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    const app = new App();
+
+    inputType.addEventListener('change', this._toggleClimbField)
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function() {
+          alert('Что то пошло не так!')
+        }
+      )
+    }
+  }
+
+  _loadMap(position) {
       const {latitude} = position.coords;
       const {longitude} = position.coords;
 
       const coords = [latitude, longitude];
 
-      console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
 
-      map = L.map('map').setView(coords, 13);
+      console.log(this);
+      this.#map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
+      }).addTo(this.#map);
 
       // Обработка клика на карте
-      map.on('click', function(event) {
-        mapEvent = event;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      })
-    },
-    function() {
-      alert('Что то пошло не так!')
-    }
-  )
+      this.#map.on('click', this._showForm.bind(this))
+  }
+
+  _showForm(e) {
+    this.#mapEvent = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleClimbField() {
+    inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+    inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Очистка полей ввода данных
+    inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
+
+    // Отображение маркера
+    const {lat, lng} = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 200,
+          minHeight: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }))
+      .setPopupContent('Тренеровка')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', function(event)  {
-  event.preventDefault();
+const app = new App();
 
-  // Очистка полей ввода данных
-  inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
 
-  // Отображение маркера
-  console.log(mapEvent);
-  const {lat, lng} = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 200,
-        minHeight: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-    }))
-    .setPopupContent('Тренеровка')
-    .openPopup();
-});
-
-inputType.addEventListener('change', function() {
-  inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
-  inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
-})
 
